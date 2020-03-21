a8f3344ec4d466a3da8176fc0657b5144dfa2fd9
PeterPang1998 <462235284@qq.com>
2020-03-21 16:41:19 +0800

    2

--- battleships.c
+++ battleships.c
@@ -259,7 +259,7 @@ int main(){
                     draw(board_hit2);
                      finished=0;
                }
-            //    printf("\n");
+                printf("\n");
             }
             row2=row2-65;
             
