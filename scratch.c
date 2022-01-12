6f07edd9726bed9c2dc27408328ef4c6bf64e7da
XiaoyuanPang98 <462235284@qq.com>
2022-01-12 09:19:19 +0800

    migrate and move the file to github

---
+++ scratch.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define C 0
+int main(){ 
+    // char list_of_ships[6][15]={"Carrier","Battleship","Destroyer","Submarine","Patrol Boat"};
+    // char name_intial[15] = list_of_ships[C][0];
+    int a=65;
+    printf("%c",a);
+    return 0;
+}
